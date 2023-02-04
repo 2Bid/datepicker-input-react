e4474394b1605acf79b2eaee0b366951333fa423
2Bid <salenbieraxel@gmail.com>
2023-02-04 20:41:24 +0100

    fix des bug lié a la transisition du projet

---
+++ src/components/input/Input.js
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+import * as React from 'react'
+import { useState, Fragment } from 'react'
+import CalendarModal from '../CalendarModal/CalendarModal'
+import styles from './input.module.css';
+
+export default function Input(props) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectDate, setSelectDate] = useState('')
+
+  const handleDateSelect = (date) => {
+    setSelectDate(date)
+  }
+
+  const closeCalendar = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <div
+      className={styles.App}
+      onClick={(e) => {
+        if (e.target.className === 'App') setIsOpen(false)
+      }}
+    >
+      <div className={styles.input__container}>
+        <input
+          value={selectDate}
+          onClick={() => {
+            setIsOpen(true)
+          }}
+        />
+        {isOpen ? (
+          <CalendarModal
+            onSelect={handleDateSelect}
+            close={closeCalendar}
+            value={selectDate}
+            home={props.home}
+          />
+        ) : (
+          <Fragment/>
+        )}
+      </div>
+    </div>
+  )
+}
